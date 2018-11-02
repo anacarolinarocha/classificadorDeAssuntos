16d4b76c0b43f9ce39da33eb2364f3a23580b9f7
anacarolinarocha <anacarolinaprm@gmail.com>
2018-11-01 23:12:33 -0200

    Adicionando script que cria a tabela tb_pte_log_extracao

--- scripts/CriaObjetosPTE.sql
+++ scripts/CriaObjetosPTE.sql
@@ -92,6 +92,134 @@ SELECT public.fn_pjekz_3772_executa();
 
 DROP FUNCTION public.fn_pjekz_3772_executa();
 DROP FUNCTION public.fn_pjekz_3772_tabela_nao_existe(nome_schema varchar, nome_tabela varchar);
+
+--##################################################################################################
+--##################################################################################################
+-- PJE_2.3.0_074__DDL_PJEKZ-3069_CRIAR_TABELA_TB_PTE_EXTRATOR_LOG
+--##################################################################################################
+--##################################################################################################
+/*
+ * Objetivo      : Criação da tabela tb_pte_log_extracao
+ * Issue         : [PJEKZ-3069]
+ * SubSistema    : (X)1ºGrau (X)2ºGrau (X)3ºGrau
+ * Descricao     : Criação da tabela tb_pte_log_extracao para guardar as informações sobre o procedimento de extração do módulo Pesquisa Textual
+ *
+ * Equipe        : CTPJE/SMPAD
+ * Issue         : [PJEKZ-3069]
+ * Autor         : Ana Carolina Pereira Rocha Martins
+ * Email		 : acprocha@tst.jus.br
+ * Data Criacao  : 12/06/2018  
+ * 
+*/
+
+CREATE OR REPLACE FUNCTION public.fn_pjekz_3069_tabela_nao_existe(nome_schema varchar, nome_tabela varchar)
+RETURNS boolean AS $BODY$
+DECLARE
+  var_table_exists smallint;
+
+BEGIN
+        SELECT 1 INTO var_table_exists
+        FROM information_schema.TABLES
+        WHERE table_schema = nome_schema
+        AND table_name     = nome_tabela;
+    
+        return (var_table_exists is null);
+END;
+$BODY$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION public.fn_pjekz_3069_executa()
+RETURNS void AS $BODY$
+BEGIN
+
+	IF  (public.fn_pjekz_3069_tabela_nao_existe('pje_pte', 'tb_pte_log_extracao')) then
+	
+		DROP SEQUENCE IF EXISTS  pje_pte.sq_tb_pte_log_extracao;
+		CREATE SEQUENCE pje_pte.sq_tb_pte_log_extracao;
+		
+		ALTER TABLE pje_pte.sq_tb_pte_log_extracao OWNER TO pje;
+		
+		GRANT SELECT, UPDATE ON SEQUENCE pje_pte.sq_tb_pte_log_extracao TO pjero_usuario_servico_pte;
+	
+		/*==============================================================*/
+		/* Table: tb_pte_log_extracao                                   */
+		/*==============================================================*/
+		create table pje_pte.tb_pte_log_extracao (
+		   id_pte_log_extracao  INTEGER              not null  default nextval('pje_pte.sq_tb_pte_log_extracao'),
+		   cd_tribunal          VARCHAR(30)          ,
+		   dh_processamento     TIMESTAMP            not null,
+		   in_sucesso           CHAR(1)              not null,
+		   		constraint tb_pte_log_extracao_cc01 check (in_sucesso in ('S','N')),
+		   ds_erro              TEXT                 null,
+		   cd_tipo_documento    VARCHAR(13)          null,
+		      	constraint tb_pte_log_extracao_cc02 check (cd_tipo_documento is null or (cd_tipo_documento in ('HTML','PDF','PDF ESCANEADO'))),
+		   hh_tempo_processamento TIME               not null,
+		   nm_host_processamento VARCHAR(255)        not null,
+		   id_processo_documento INTEGER             not null,
+		   constraint tb_pte_log_extracao_pk primary key (id_pte_log_extracao)
+		);
+		
+		comment on table pje_pte.tb_pte_log_extracao is
+		'Tabela que armazena as informações de log do extrator do módulo Pesquisa Textual';
+		
+		comment on column pje_pte.tb_pte_log_extracao.id_pte_log_extracao is
+		'Identificador único da tabela';
+		
+		comment on column pje_pte.tb_pte_log_extracao.cd_tribunal is
+		'Código do Tribunal de onde provem o dado';
+		
+		comment on column pje_pte.tb_pte_log_extracao.dh_processamento is
+		'Data do processamento';
+		
+		comment on column pje_pte.tb_pte_log_extracao.in_sucesso is
+		'Indicador se houve sucesso ou falha no procedimento de extração';
+		
+		comment on column pje_pte.tb_pte_log_extracao.ds_erro is
+		'Descrição do erro ocorrido no procedimento de extração';
+		
+		comment on column pje_pte.tb_pte_log_extracao.cd_tipo_documento is
+		'Tipo do documento carregado';
+		
+		comment on column pje_pte.tb_pte_log_extracao.hh_tempo_processamento is
+		'Tempo decorrido (em segundos) no procedimento de extração';
+		
+		comment on column pje_pte.tb_pte_log_extracao.nm_host_processamento is
+		'Nome do host de onde se desparou a extração';
+		
+		comment on column pje_pte.tb_pte_log_extracao.id_processo_documento is
+		'Id do documento carregado';
+
+		-- set table ownership
+		alter table pje_pte.tb_pte_log_extracao owner to pje;
+	
+		grant DELETE,INSERT,SELECT,UPDATE on pje_pte.tb_pte_log_extracao to pjero_usuario_manutencao;
+
+		grant DELETE,INSERT,SELECT,UPDATE on pje_pte.tb_pte_log_extracao to pjero_usuario_servico;
+
+		/*==============================================================*/
+		/* Index: tb_pte_log_extracao_in01                              */
+		/*==============================================================*/
+		create  index tb_pte_log_extracao_in01 on pje_pte.tb_pte_log_extracao (
+		dh_processamento
+		);
+		
+		/*==============================================================*/
+		/* Index: tb_pte_log_extracao_in02                              */
+		/*==============================================================*/
+		create  index tb_pte_log_extracao_in02 on pje_pte.tb_pte_log_extracao (
+		ds_erro
+		);
+
+	END IF;
+	
+END;
+$BODY$ LANGUAGE plpgsql;
+
+SELECT public.fn_pjekz_3069_executa();
+
+DROP FUNCTION public.fn_pjekz_3069_executa();
+
+
+
 --##################################################################################################
 --##################################################################################################
 -- PJE_2.3.0_151__DDL_CTPJE-8599_REPLACE_VIEW_VS_PTE_DOCUMENTO_INDEXAVEL
@@ -173,7 +301,8 @@ BEGIN
 		  	AND ((select grau.vl_variavel from grau) = '3' or 
 				(doc.ds_instancia='3' OR doc.ds_instancia::text =(select grau.vl_variavel from grau)))
 		  	AND ptrf.cd_processo_status = 'D'::bpchar
-		    AND pa.in_assunto_principal = 'S' and ptrf.in_segredo_justica = 'N' and doc.in_documento_sigiloso = 'N';
+		    AND pa.in_assunto_principal = 'S' and ptrf.in_segredo_justica = 'N' and doc.in_documento_sigiloso = 'N'
+			AND doc.id_tipo_processo_documento = (SELECT id_tipo_processo_documento FROM pje.tb_tipo_processo_documento WHERE ds_tipo_processo_documento = 'Recurso Ordinário');
            
 
 	ALTER TABLE pje_pte.vs_pte_documento_indexavel
@@ -190,4 +319,3 @@ $BODY$ LANGUAGE plpgsql;
 SELECT public.fn_ctpje_8599_executa();
 
 DROP FUNCTION public.fn_ctpje_8599_executa();
-
