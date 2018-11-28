63678000fe699f1b51edb473e610260d7c537dfb
anacarolinarocha <anacarolinaprm@gmail.com>
2018-11-28 00:03:44 -0200

    Adicionando versao estavel do código junto ao experimento, just in case

---
+++ Resultados/Experimento 14 - TRT13_2G_JT_REDUZIDO_LSI_250_CorpusCompleto_CV5/modelo.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 27 16:08:24 2018
+
+@author: anarocha
+"""
+
+class Modelo:
+    def __init__(self, nome, feature_type, tempo_processamento, best_params_, best_estimator_, grid_scores_, macro_precision, macro_recall, macro_fscore, micro_precision, micro_recall, micro_fscore):  
+        self.nome = nome
+        self.feature_type = feature_type
+        self.tempo_processamento = tempo_processamento
+        self.best_params_ = best_params_
+        self.best_estimator_ = best_estimator_
+        self.grid_scores_ = grid_scores_
+        self.macro_precision = macro_precision
+        self.macro_recall = macro_recall
+        self.macro_fscore = macro_fscore
+        self.micro_precision = micro_precision
+        self.micro_recall = micro_recall
+        self.micro_fscore = micro_fscore
+     
+    def setNome(self, nome):
+        self.nome = nome
+        
+    def setFeatureType(self, feature_type):
+        self.feature_type = feature_type
+        
+    def setTempoProcessamento(self, tempo_processamento):
+        self.tempo_processamento = tempo_processamento
+     
+    def setBestParams(self, best_params_):
+        self.best_params_ = best_params_
+    
+    def setBestEstimator(self, best_estimator_):
+        self.best_estimator_ = best_estimator_
+        
+    def setGridScores(self, grid_scores_):
+        self.grid_scores_ = grid_scores_
+        
+    def setMacroPrecision(self, macro_precision):
+        self.macro_precision = macro_precision
+     
+    def setMacroRecall(self, macro_recall):
+        self.macro_recall = macro_recall
+        
+    def setMacroFscore(self, macro_fscore):
+        self.macro_fscore = macro_fscore
+        
+    def setMicroPrecision(self, micro_precision):
+        self.micro_precision = micro_precision
+     
+    def setMicroRecall(self, micro_recall):
+        self.micro_recall = micro_recall
+        
+    def setMicroFscore(self, micro_fscore):
+        self.micro_fscore = micro_fscore
+     
+    def getNome(self):
+        return self.nome
+    
+    def getFeatureType(self):
+        return self.feature_type
+    
+    def getTempoProcessamento(self):
+        return self.tempo_processamento
+         
+    def getBestParams(self):
+        return self.best_params_
+    
+    def getBestEstimator(self):
+        return self.best_estimator_ 
+    
+    def getGridScores(self, grid_scores_):
+        return self.grid_scores_ 
+    
+    def getMacroPrecision(self):
+        return self.macro_precision 
+     
+    def getMacroRecall(self):
+        return self.macro_recall 
+        
+    def getMacroFscore(self):
+        return self.macro_fscore 
+        
+    def getMicroPrecision(self):
+        return self.micro_precision 
+     
+    def getMicroRecall(self):
+        return self.micro_recall 
+        
+    def getMicroFscore(self):
+        return self.micro_fscore 
+        
+ 
